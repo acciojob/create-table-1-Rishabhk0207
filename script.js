16294548fd07f583463bb7e23fcc126b1d6373ae
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-28 21:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let table = document.querySelector('#sampleTable');
+
 function insert_Row() {
     //Write your code here
-  
-  
+  let row= document.createElement('tr');
+  let col= document.createElement('td');
+  col.textContent ='New Cell1';
+  row.appendChild(col);
+  col= document.createElement('td');
+  col.textContent= 'New Cell2';
+  row.appendChild(col);
+
+  table.insertAdjacentElement('afterBegin',row);
 }
