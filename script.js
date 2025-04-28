d3556a9e47c12cac27f6d145b844d0e04000b1f4
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2025-04-28 21:43:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 let table = document.querySelector('#sampleTable');
-let rowCount = 1;
+
 function insert_Row() {
     //Write your code here
-  let row = table.insertRow(0); // Insert new row at top
-
-  let cell1 = row.insertCell(0);
-  let cell2 = row.insertCell(1);
-
-  cell1.textContent = `New Cell1Row${rowCount} cell1`;
-  cell2.textContent = `New Cell2Row${rowCount} cell2`;
-
-  rowCount++; // Increment for next row
+ let row = table.insertRow(0); // Insert a row at the top (index 0)
+  let cell1 = row.insertCell(0); // Insert a cell at position 0
+  let cell2 = row.insertCell(1); // Insert a cell at position 1
+  
+  cell1.textContent = 'New Cell 1';
+  cell2.textContent = 'New Cell 2';
 }
